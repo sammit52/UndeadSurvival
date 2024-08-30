27cc052bc2064e6c4a3b169502c90a01942bb297
sammit52 <sammit52@kingshigh.school.nz>
2024-08-30 15:06:37 +1200

    Shotgun Guns Added And Bug Fixes

--- Code/player.gd
+++ Code/player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
 @export var speed: float = 200.0  # Movement speed in pixels per second
-@export var bullet_scene : PackedScene
+@export var assault_bullet_scene : PackedScene
+@export var shotgun_bullet_scene : PackedScene
 @onready var marker_2d: Marker2D = $Marker2D
 var guns = {0:["Pistol",5,99,false,0.0,0,400],1:["Gun1",2,50,true,0.075,7.5,225],2:["Gun2",2,50,true,0.2,1.5,400],3:["Gun3",2,50,true,0.2,2,400],4:["Gun4",2,50,true,0.2,1,400],5:["Gun5",2,50,true,0.2,1,400],6:["Gun6",2,50,true,0.2,1,400],7:["Gun7",2,50,true,0.2,1,400],8:["Gun8",2,50,true,0.2,1,400],9:["Gun9",2,50,true,0.2,1,400]}
 #name, damage, max ammo, automatic?, time, 
 #var gun_names = ["Pistol", "Gun1", "Gun2", "Gun3", "Gun4", "Gun5", "Gun6", "Gun7", "Gun8", "Gun9"] # List of gun animation names
-
+var unlocked_gun_index = 0
 var unlocked_guns = [0,1,2,3,4]
 var can_shoot = true
 var current_gun_index = 0
+
 
 # Reference to the AnimatedSprite2D node
 @onready var gun_sprite = $Guns
@@ -22,6 +24,7 @@ func equip_gun(index: int):
 	
 	#if index >= 0 and index <= unlocked_guns.size():
 	gun_sprite.animation = guns[index][0]
+
 
 func _process(delta: float) -> void:
 	var velocity: Vector2 = Vector2.ZERO
@@ -67,29 +70,46 @@ func _process(delta: float) -> void:
 
 	# Scroll wheel to switch guns
 	if Input.is_action_just_pressed("ui_scroll_up"):
-		current_gun_index += 1
-		if current_gun_index >= unlocked_guns.size():
-			current_gun_index = 0
-		
-		equip_gun(unlocked_guns[current_gun_index])
+		unlocked_gun_index += 1
+		if unlocked_gun_index >= unlocked_guns.size():
+			unlocked_gun_index = 0
+		current_gun_index = unlocked_guns[unlocked_gun_index]
+		equip_gun(unlocked_guns[unlocked_gun_index])
 	
 	if Input.is_action_just_pressed("ui_scroll_down"):
-		current_gun_index -= 1
-		if current_gun_index < 0:
-			current_gun_index = unlocked_guns.size() - 1
-		equip_gun(unlocked_guns[current_gun_index])
+		unlocked_gun_index -= 1
+		if unlocked_gun_index < 0:
+			unlocked_gun_index = unlocked_guns.size() - 1
+		current_gun_index = unlocked_guns[unlocked_gun_index]
+		equip_gun(unlocked_guns[unlocked_gun_index])
+	
+	# Was using current gun index for two different things, the keypad to define what gun is equipped, and with this old code to scroll through the unlocked gun index and not what gun is equipped, unlocked gun index should be the gun thats equipped.
+	#if Input.is_action_just_pressed("ui_scroll_down"):
+		#current_gun_index -= 1
+		#if current_gun_index < 0:
+			#current_gun_index = unlocked_guns.size() - 1
+		#equip_gun(unlocked_guns[current_gun_index])
+
 
 	
 	if Input.is_action_pressed("shoot") and can_shoot:
 		can_shoot = false
+
+		#Timer for automatic guns
 		if guns[current_gun_index][3]:
 			$Timer.wait_time = guns[current_gun_index][4]
 			$Timer.start()
-		var bullet = bullet_scene.instantiate()
+		var bullet
+		#checking if shotgun or not, probably a better way to do this.
+		if current_gun_index not in [4 , 7]:
+			bullet = assault_bullet_scene.instantiate()
+		else:
+			bullet = shotgun_bullet_scene.instantiate()
 		bullet.variance = guns[current_gun_index][5]
 		bullet.speed = guns[current_gun_index][6]
 		bullet.bullet_damage = guns[current_gun_index][1]
 		bullet.global_transform = marker_2d.global_transform
+		#Changed this code for the stuff above
 		#bullet.direction = (get_global_mouse_position() - position).normalized()
 		#bullet.look_at(get_global_mouse_position())
 		add_sibling(bullet)
@@ -98,6 +118,7 @@ func _process(delta: float) -> void:
 		can_shoot = true
 		
 func _input(event):
+	# Changing weapons using number keys
 	if event is InputEventKey and event.is_pressed():
 		if event.keycode not in [KEY_0,KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7,KEY_8,KEY_9]:
 			return
@@ -129,6 +150,6 @@ func _input(event):
 		if gun_select in unlocked_guns:
 			equip_gun(gun_select)
 
-
+# Automatic gun stuff
 func _on_timer_timeout() -> void:
 	can_shoot = true
